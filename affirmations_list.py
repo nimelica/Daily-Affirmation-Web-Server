0f553d1b18351c2df9888a63a98dec6229948b30
Nimet Ozkan <melaht2016@icloud.com>
2022-02-15 15:22:03 -0600

    added dependency files

--- affirmations_list.py
+++ affirmations_list.py
@@ -21,11 +21,11 @@ connection = sqlite3.connect('affirmations_list.db')
 cursor = connection.cursor()
 
 # Create table/s for our database
-cursor.execute("create table drinks (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+cursor.execute("create table affirmations (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
 
 # Fill that table
 for i in range(len(affirmations)):
-  cursor.execute("insert into drinks (name) values (?)",[affirmations[i]])
+  cursor.execute("insert into affirmations (name) values (?)",[affirmations[i]])
   print("added to table->", affirmations[i])
 
 # Do not forget to save (commit) and close the database connection
--- app.py
+++ app.py
@@ -8,4 +8,42 @@ app.secret_key = "12ev@0947!fBjs8AA#4$32jHbd_sjhAdhc%mhn_773gvPPahG/HSI*IH_jbs"
 # Main page
 @app.route('/', methods=['GET'])
 def index():
-    return "HELLO"
+    flash("AFFIRMATION FOR TODAY!")
+    return render_template("index.html")
+
+
+@app.route('/affirmation', methods=['GET', 'POST'])
+def affirmation():
+    sentence = get_db()
+    return render_template("index.html", sentence=sentence)
+
+
+# Managing database connection
+# g is to manage resources during a request
+# During a request, every call to get_db() will return the same connection, 
+# and it will be closed automatically at the end of the request
+
+# this function is connecting our database to our application
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('affirmations_list.db')
+        cursor = db.cursor()
+        cursor.execute("select name from affirmations")
+        all_data = cursor.fetchall() # keeps all names in tuples
+        all_names = [str(val[0]) for val in all_data] # hold names only
+        random_affirmation = random.choice(all_names)
+    return random_affirmation
+
+# this function is terminating our database connection
+# once we are done with using it
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+# run app
+if __name__ == '__main__':
+	app.run(debug=True)
